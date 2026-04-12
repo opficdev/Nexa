8c365c2f400370a3e360860d50478fd0be6168e0
opficdev <opficdev@gmail.com>
2026-04-12 14:24:42 +0900

    chore: 플랫폼 및 Swift 버전 6.1로 수정

--- Nexa/Package.swift
+++ Nexa/Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version: 6.3
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Nexa",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
