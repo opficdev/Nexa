e090bc9472d7a21802d5c199cd3dcf28910e82b7
opficdev <opficdev@gmail.com>
2026-04-13 14:51:24 +0900

    feat: 요청 실행 API와 인터셉터 체인 연결 구현

---
+++ Nexa/Sources/Nexa/Runtime/NXRequestExecutor.swift
@@ -0,0 +1,83 @@
+//
+//  NXRequestExecutor.swift
+//  Nexa
+//
+//  Created by 최윤진 on 4/12/26.
+//
+
+import Foundation
+
+enum NXRequestExecutor {
+    static func executeRaw(
+        clientConfiguration: NXClientConfiguration,
+        requestSpec: RequestSpec
+    ) async throws -> NXRawResponse {
+        do {
+            let request = try NXRequestAssembler.assemble(
+                clientConfiguration: clientConfiguration,
+                requestSpec: requestSpec
+            )
+            let context = NXRequestExecutionContext(
+                request: request,
+                requestIdentifier: requestSpec.requestIdentifier,
+                attemptNumber: 1,
+                userInfo: requestSpec.userInfo,
+                specification: requestSpec,
+                clientConfiguration: clientConfiguration
+            )
+            let rawResponse = try await NXInterceptorChain.execute(
+                context: context,
+                interceptors: runtimeInterceptors(
+                    clientConfiguration: clientConfiguration,
+                    requestSpec: requestSpec
+                ),
+                transport: clientConfiguration.transport
+            )
+
+            try NXResponsePipeline.validate(
+                clientConfiguration: clientConfiguration,
+                requestSpec: requestSpec,
+                rawResponse: rawResponse
+            )
+
+            return rawResponse
+        } catch {
+            throw NXResponsePipeline.map(error: error)
+        }
+    }
+
+    static func executeDecode<T: Decodable>(
+        clientConfiguration: NXClientConfiguration,
+        requestSpec: RequestSpec,
+        responseType: T.Type
+    ) async throws -> T {
+        let rawResponse = try await executeRaw(
+            clientConfiguration: clientConfiguration,
+            requestSpec: requestSpec
+        )
+
+        do {
+            return try NXResponsePipeline.decode(
+                clientConfiguration: clientConfiguration,
+                rawResponse: rawResponse,
+                responseType: responseType
+            )
+        } catch {
+            throw NXResponsePipeline.map(error: error)
+        }
+    }
+
+    private static func runtimeInterceptors(
+        clientConfiguration: NXClientConfiguration,
+        requestSpec: RequestSpec
+    ) -> [any NXHTTPInterceptor] {
+        var interceptors: [any NXHTTPInterceptor] = [
+            NXRetryInterceptor(),
+            NXAuthInterceptor(),
+        ]
+        interceptors.append(contentsOf: clientConfiguration.interceptors)
+        interceptors.append(contentsOf: requestSpec.requestInterceptors)
+        interceptors.append(NXLoggerInterceptor())
+        return interceptors
+    }
+}
