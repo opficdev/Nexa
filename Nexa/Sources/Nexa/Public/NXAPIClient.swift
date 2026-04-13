987144f5a0ae668c7874465c3c64249072fc3994
opficdev <opficdev@gmail.com>
2026-04-13 10:36:47 +0900

    feat: 선언형 요청 진입 클라이언트 구조 구현

---
+++ Nexa/Sources/Nexa/Public/NXAPIClient.swift
@@ -0,0 +1,44 @@
+//
+//  NXAPIClient.swift
+//  Nexa
+//
+//  Created by 최윤진 on 4/13/26.
+//
+
+import Foundation
+
+public struct NXAPIClient: Sendable {
+    let clientConfiguration: NXClientConfiguration
+
+    public init(configuration: NXClientConfiguration) {
+        clientConfiguration = configuration
+    }
+
+    public func get(_ path: String) -> NXRequestBuilder {
+        request(method: .get, path: path)
+    }
+
+    public func post(_ path: String) -> NXRequestBuilder {
+        request(method: .post, path: path)
+    }
+
+    public func put(_ path: String) -> NXRequestBuilder {
+        request(method: .put, path: path)
+    }
+
+    public func patch(_ path: String) -> NXRequestBuilder {
+        request(method: .patch, path: path)
+    }
+
+    public func delete(_ path: String) -> NXRequestBuilder {
+        request(method: .delete, path: path)
+    }
+
+    public func request<E: NXEndpoint>(_ endpoint: E) -> NXRequestBuilder {
+        endpoint.configure(request(method: endpoint.method, path: endpoint.path))
+    }
+
+    func request(method: NXHTTPMethod, path: String) -> NXRequestBuilder {
+        NXRequestBuilder(clientConfiguration: clientConfiguration, requestSpec: RequestSpec(method: method, path: path))
+    }
+}
