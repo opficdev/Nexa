988edf56a9419b9c5a6a6f3d30173c9e78eac206
opficdev <162981733+opficdev@users.noreply.github.com>
2026-08-25 10:32:32 +0900

    docs: cache와 retry 주석 재구성

--- Sources/Core/NXRetryJitter.swift
+++ Sources/Core/NXRetryJitter.swift
@@ -5,10 +5,10 @@
 //  Created by opfic on 8/23/26.
 //
 
-/// 로컬 재시도 백오프 지연에 적용하는 무작위화입니다.
+/// 로컬 retry backoff delay 무작위화
 public enum NXRetryJitter: Sendable, Equatable {
-    /// 로컬 백오프 지연을 변경하지 않습니다.
+    /// 로컬 backoff delay 비변경
     case none
-    /// 로컬 백오프 지연 범위 내의 임의 값을 사용합니다.
+    /// 로컬 backoff delay 범위 임의값 사용
     case full
 }
